9581f05c79b66172d14a780676e1f702d26a4ef8
Zouhair <rouikazouhair@gmail.com>
2022-05-17 23:02:05 +0100

    Adding a new video

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -23,25 +23,43 @@ const vars: Ref<YT.PlayerVars> = ref({
   loop: 1,
 })
 
-const timeStampsPerVideo: Ref<Video[]> = ref([
+const timeStampsPerVideo: Ref<any[]> = ref([
   {
-    id: 'iVChyaHLH68',
+    id: 'BDllaGN-4nY',
+    title: 'Video 1',
     loop: 1,
-    start: 1195,
-    end: 1200,
     autoplay: 1,
+    timestamps: [
+      {
+        name: '5th 1/8',
+        start: 872,
+        end: 1082,
+      },
+      {
+        name: '6th 1/8',
+        start: 1082,
+        end: 1292,
+      },
+    ],
   },
 ])
 
 const submitVideo = async () => {
-  const test = await axios.get(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${newVideoId.value}&key=AIzaSyCwk7uCR8zZLOg38CuqUg5aZHmlmbRvkDk`)
+  const videoResp = await axios.get(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${newVideoId.value}&key=AIzaSyCwk7uCR8zZLOg38CuqUg5aZHmlmbRvkDk`)
+
+  timeStampsPerVideo.value.push({
+    id: newVideoId.value,
+    title: videoResp.data?.items?.[0].snippet.title,
+    loop: 1,
+    autoplay: 1,
+    timestamps: [],
+  })
 
   newVideoId.value = ''
-
-  console.log(test.data?.items?.[0].snippet.title)
 }
 
 const start = (timestamp: Video) => {
+  console.log(timestamp)
   showFrame.value = false
   src.value = `${BASE_URL}=${timestamp.id}`
   vars.value = {
@@ -53,17 +71,33 @@ const start = (timestamp: Video) => {
 
   setTimeout(() => {
     showFrame.value = true
-    youtube.value.unMute()
   }, 500)
+
+  setTimeout(() => {
+    youtube.value.unMute()
+  }, 1500)
 }
 
 const StateChange = (state: any) => {
   if (state.data === 0) {
     state.target.playVideo()
-    state.target.seekTo(10, true)
+    state.target.seekTo(vars.value.start, true)
   }
 }
 
+const currentVideo: Ref<number | null> = ref(null)
+const selectVideo = (index: number) => currentVideo.value = index
+
+/* <button v-if="video.timestamps.length === 0" @click="start(video)">
+  Start
+</button>
+
+<div v-for="(timestamp, key) in video.timestamps" v-else :key="key">
+  <p>{{ timestamp.name }} || start: {{ timestamp.start }} || end: {{ timestamp.end }}</p>
+  <button @click="start({ ...video, timestamp })">
+    Start
+  </button>
+</div> */
 onBeforeMount(async () => {
 })
 </script>
@@ -85,11 +119,47 @@ onBeforeMount(async () => {
       @state-change="StateChange"
     />
 
-    <div v-for="timestamp in timeStampsPerVideo" :key="timestamp.id">
-      <p>{{ timestamp.id }}</p>
-      <button @click="start(timestamp)">
+    <div v-for="(video, index) in timeStampsPerVideo" :key="video.id">
+      <p>{{ video.title }}</p>
+      <button @click="selectVideo(index)">
+        Select
+      </button>
+    </div>
+
+    <div v-if="currentVideo">
+      <button v-if="timeStampsPerVideo[currentVideo].timestamps.length === 0" @click="start(timeStampsPerVideo[currentVideo || -1])">
         Start
       </button>
+
+      <div v-for="(timestamp, key) in timeStampsPerVideo[currentVideo].timestamps" v-else :key="key" class="bg-white">
+        <p>{{ timestamp.name }} || start: {{ timestamp.start }} || end: {{ timestamp.end }}</p>
+        <button @click="start({ ...video, timestamp })">
+          Start
+        </button>
+        <button
+          @click="timeStampsPerVideo[currentVideo || 0].timestamps.push(
+            {
+              name: '',
+              end: '',
+              start: '',
+              autoplay: 1,
+            },
+          )"
+        >
+          Add
+        </button>
+        <div>
+          <p>Name</p>
+          <input v-model="timestamp.name" type="text">
+          <p>Start in seconds</p>
+          <input v-model="timestamp.start" type="text">
+          <p>End in seconds</p>
+          <input v-model="timestamp.end" type="text">
+          <button @click="start({ ...video, timestamp })">
+            Add
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
