e34767086b90fdd514b543ebff9eb42b1b5c62ad
Zouhair <rouikazouhair@gmail.com>
2022-05-18 16:09:44 +0100

    Revert "Test netlify npm instead of pnpm"
    This reverts commit 99f36c4e7604251d1046b6336ff9f399c3810298.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
+import { ViteSSG } from 'vite-ssg'
 import { setupLayouts } from 'virtual:generated-layouts'
 import Naive from 'naive-ui'
-import { createHead } from '@vueuse/head'
-import { createI18n } from 'vue-i18n'
-import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 import generatedRoutes from '~pages'
 
@@ -13,7 +11,7 @@ import 'uno.css'
 const routes = setupLayouts(generatedRoutes)
 
 // https://github.com/antfu/vite-ssg
-/* export const createApp = ViteSSG(
+export const createApp = ViteSSG(
   App,
   { routes, base: import.meta.env.BASE_URL },
   (ctx) => {
@@ -22,34 +20,4 @@ const routes = setupLayouts(generatedRoutes)
     ctx.app.use(Naive)
     Object.values(import.meta.globEager('./modules/*.ts')).forEach(i => i.install?.(ctx))
   },
-) */
-
-const messages = Object.fromEntries(
-  Object.entries(
-    import.meta.globEager('../../locales/*.y(a)?ml'))
-    .map(([key, value]) => {
-      const yaml = key.endsWith('.yaml')
-      return [key.slice(14, yaml ? -5 : -4), value.default]
-    }),
 )
-
-const i18n = createI18n({
-  legacy: false,
-  locale: 'en',
-  messages,
-})
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-})
-
-const head = createHead()
-
-const app = createApp(App)
-app.use(router)
-app.use(head)
-app.use(i18n)
-app.use(Naive)
-app.mount('#app')
-
